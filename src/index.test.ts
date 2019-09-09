3bcedacb10fa19a2cb8ba9e666d1b49baae733eb
teramotodaiki <i101323@gmail.com>
2019-09-09 11:53:50 +0900

    fix: add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import test from 'ava';
 import { createLogger } from '.';
+import g from 'global';
 
 test('Create with same keys', t => {
   const logger1 = createLogger();
@@ -13,4 +14,9 @@ test('Create with different keys', t => {
   const logger2 = createLogger('2');
 
   t.not(logger1, logger2);
+});
+
+test('Check default global var name', t => {
+  const logger = createLogger();
+  t.is(logger, g['Unique primary key used by @hackforplay/log']);
 });
